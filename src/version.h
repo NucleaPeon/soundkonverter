dc4d7eb6cc3c8bbacde36a1cd0ad5ec05988721a
Daniel Kettle <initial.dann@gmail.com>
2026-07-12 10:28:41 -0600

    Changes to CMakeLists.txt: Qt6/KF6 and version.h/project call updates

---
+++ src/version.h
@@ -0,0 +1,20 @@
+#ifndef SOUNDKONVERTER_VERSION_H
+#define SOUNDKONVERTER_VERSION_H
+
+// Compiler accessible definitions
+
+#define SOUNDKONVERTER_VERSION_RELEASE "3"
+#define SOUNDKONVERTER_VERSION_MAJOR "0"
+#define SOUNDKONVERTER_VERSION_MINOR "1"
+
+// Program accessible definitions
+
+namespace SoundKonverter {
+    namespace Version {
+        const int RELEASE = 3;
+        const int MAJOR = 0;
+        const int MINOR = 1;
+    }
+}
+
+#endif // SOUNDKONVERTER_VERSION_H
